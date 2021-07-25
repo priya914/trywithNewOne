64e1a827062a5fa36c28dfbae9af3486005c258c
su-ui <66843665+su-ui@users.noreply.github.com>
2021-07-25 00:30:31 -0500

    first commitgit branch

--- src/main/java/addTwoNumbers.java
+++ src/main/java/addTwoNumbers.java
@@ -5,8 +5,12 @@ public class addTwoNumbers {
         int a=1,b=02;
 
         int c = a+b;
+	@FindBy(css="")
+	private WebElement webElement;
 
         System.out.println(c);
+        
+        
 
     }
 }
