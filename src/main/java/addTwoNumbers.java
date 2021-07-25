6461fcaa78291a8fd8fffe2ec4d94d3134a96c44
su-ui <66843665+su-ui@users.noreply.github.com>
2021-07-25 00:11:02 -0500

    added new code

--- src/main/java/addTwoNumbers.java
+++ src/main/java/addTwoNumbers.java
@@ -2,11 +2,12 @@ public class addTwoNumbers {
 
     public static void main(String[] args)
     {
-        int a=1,b=02;
+        int a=1,b=2;
 
         int c = a+b;
 
         System.out.println(c);
+
 
     }
 }
---
+++ src/test/java/Palindrome.java
@@ -0,0 +1,12 @@
+public class Palindrome {
+
+    public void add()
+    {
+        System.out.println("it is a new method");
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("new added code");
+    }
+}
